66b408bc830f4361231e5f292a1fdd6fb1bac285
amos7121 <bognetamos@gmail.com>
2026-07-28 11:19:21 +0100

    upload first version of berashit foundation web
    practise with html.index, style.css and app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("Hello Class");
+alert("Welcome to my website");
+
+let name = "Bognet";
+let score = 58;
+let isPresent = true;
+
+
+console.log (name);
+console.log (score);
+console.log (isPresent)
+
+
+
+function greetUser(params) {
+    alert("Good Morning"+ name);
+}
+
+greetUser();
+
+let Messagebtn = document.getElementById (Messagebtn);
+Messagebtn.addEventListener(click, function name(params) {
+alert ("button clicked")    
+}
+
+)
